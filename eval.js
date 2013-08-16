201fefec624b1da018cf2a266a0632f37f4e4259
Scott Feeney <scott@oceanbase.org>
2013-08-16 19:43:39 -0400

    Add eval and tests

--- eval.js
+++ eval.js
@@ -1,3 +1,111 @@
-exports.evalLisp = function(tree) {
-    console.log('not implemented yet');
+function shallowCopy(obj) {
+    var clone = {};
+    Object.keys(obj).forEach(function(key) {
+        clone[key] = obj[key];
+    });
+    return clone;
 }
+
+function has(dict, key) {
+    return Object.prototype.hasOwnProperty.call(dict, key);
+}
+
+// Returns true if the value is truthy for Lisp's purposes.
+function lispTruthy(value) {
+    return value !== false && value !== null && value !== undefined;
+}
+
+function LispLambda(bindings, paramNames, body) {
+    this.bindings = shallowCopy(bindings);
+    this.paramNames = paramNames;
+    this.body = body;
+}
+
+LispLambda.prototype = {};
+// Apply method so we can call LispLambdas like regular functions.
+LispLambda.prototype.apply = function(_, args) {
+    var innerBindings = shallowCopy(this.bindings);
+    this.paramNames.forEach(function(param, index) {
+        innerBindings[param] =
+            (args[index] !== undefined) ? args[index] : null;
+    });
+    return _evalLisp(this.body, innerBindings);
+};
+
+var builtins = {
+    '+': function(a, b) { return a + b; },
+    '-': function(a, b) { return a - b; },
+    'eq?': function(a, b) { return a === b; },
+    'not': function(a) { return !lispTruthy(a); },
+    'number?': function(a) { return typeof a === 'number'; },
+    'string?': function(a) { return typeof a === 'string'; },
+    '*': function(a, b) { return a * b; },
+    '/': function(a, b) { return a / b; },
+    '<': function(a, b) { return a < b; },
+    '>': function(a, b) { return a > b; },
+    '<=': function(a, b) { return a <= b; },
+    '>=': function(a, b) { return a >= b; }
+};
+
+function _evalLisp(tree, bindings) {
+    if (bindings === undefined)
+        bindings = {};
+
+    if (typeof tree === 'number')
+        return tree;
+    else if (typeof tree === 'string') {
+        if (tree === '#t')
+            return true;
+        if (tree === '#f')
+            return false;
+        if (!has(bindings, tree))
+            throw new Error('Unbound var: ' + tree);
+        return bindings[tree];
+    }
+
+    // We have a list. Look at the thing in root position.
+    if (tree[0] === 'quote')
+        return tree[1];
+    if (tree[0] === 'if') {
+        var test = _evalLisp(tree[1], bindings);
+        if (lispTruthy(test))
+            return _evalLisp(tree[2], bindings);
+        return _evalLisp(tree[3], bindings);
+    }
+    if (tree[0] === 'set!') {
+        var varName = tree[1];
+        if (!has(bindings, varName))
+            throw new Error('Attempt to set! unbound var: ' + varName);
+        bindings[varName] = _evalLisp(tree[2], bindings);
+        return null;
+    }
+    if (tree[0] === 'begin') {
+        var result;
+        tree.forEach(function(exp, index) {
+            if (index !== 0)
+                result = _evalLisp(exp, bindings);
+        });
+        return result;
+    }
+    if (tree[0] === 'define') {
+        var varName = tree[1], value = _evalLisp(tree[2], bindings);
+        bindings[varName] = value;
+        return null;
+    }
+    if (tree[0] === 'lambda')
+        return new LispLambda(bindings, tree[1], tree[2]);
+
+    // tree[0] is a function we'll be calling.
+    // It could be a builtin, or something that when eval'd yields a
+    // function.
+    var func = builtins[tree[0]] || _evalLisp(tree[0], bindings);
+    var args = tree.slice(1).map(function(exp) {
+        return _evalLisp(exp, bindings);
+    });
+    if (func instanceof Function || func instanceof LispLambda)
+        return func.apply(func, args);
+
+    throw new Error("Attempt to call non-function: " + func.toString());
+}
+
+exports.evalLisp = _evalLisp;
---
+++ spec/evalSpec.js
@@ -0,0 +1,144 @@
+var evalLisp = require('../eval').evalLisp;
+
+describe('evaluation of scalars', function() {
+    it('returns numbers unchanged', function() {
+        expect(evalLisp(192)).toEqual(192);
+    });
+    it('looks up the value of variables', function() {
+        expect(evalLisp('foo', {'foo': 42})).toEqual(42);
+    });
+    it('throws if you use an unbound variable', function() {
+        expect(function() {
+            evalLisp('bar', {'foo': 42});
+        }).toThrow();
+    });
+    it('accepts boolean values', function() {
+        expect(evalLisp('#t')).toEqual(true);
+        expect(evalLisp('#f')).toEqual(false);
+    });
+});
+
+describe('quoting', function() {
+    it('quotes numbers', function() {
+        expect(evalLisp(['quote', 42])).toEqual(42);
+    });
+    it('quotes strings', function() {
+        expect(evalLisp(['quote', 'lambda'])).toEqual('lambda');
+        expect(evalLisp(['quote', 'foobar'])).toEqual('foobar');
+    });
+    it('quotes lists', function() {
+        expect(evalLisp(['quote', ['a', 'b', 'c']])).toEqual(['a', 'b', 'c']);
+    });
+});
+
+describe('if statement', function() {
+    it('evaluates the first thing if expression is true', function() {
+        expect(evalLisp(['if', '#t', 1, 2])).toEqual(1);
+        expect(evalLisp(['if', ['quote', 'a', 'b'], 1, 2])).toEqual(1);
+    });
+    it('doesn\'t evaluate second thing if expression is true', function() {
+        expect(evalLisp(['if', '#t', 1, 'unbound-var'])).toEqual(1);
+    });
+    it('evaluates the second thing if expression is false', function() {
+        expect(evalLisp(['if', '#f', 1, 2])).toEqual(2);
+    });
+    it('doesn\'t evaluate first thing if expression is false', function() {
+        expect(evalLisp(['if', '#f', 'unbound-var', 2])).toEqual(2);
+    });
+    it('treats 0 and empty list as true', function() {
+        expect(evalLisp(['if', 0, 1, 2])).toEqual(1);
+        expect(evalLisp(['if', ['quote', []], 1, 2])).toEqual(1);
+    });
+    it('understands predicates', function() {
+        expect(evalLisp(['if', ['eq?', 0, 0], 1, 2])).toEqual(1);
+        expect(evalLisp(['if', ['eq?', 0, 2], 1, 2])).toEqual(2);
+    });
+});
+
+describe('builtin functions', function() {
+    it('do basic math', function() {
+        expect(evalLisp(['+', 2, 10])).toEqual(12);
+        expect(evalLisp(['-', 20, 100])).toEqual(-80);
+        expect(evalLisp(['*', 0.5, 200])).toEqual(100);
+        expect(evalLisp(['/', 48, 8])).toEqual(6);
+    });
+    it('do recursive math', function() {
+        var expr = ['/', ['*', 10, 7],
+                         ['+', 4, 1]];
+        expect(evalLisp(expr)).toEqual(14);
+    });
+    it('negate booleans', function() {
+        expect(evalLisp(['not', '#t'])).toEqual(false);
+        expect(evalLisp(['if', ['not', '#f'], 1, 2])).toEqual(1);
+    });
+    it('respect truthiness of numbers and lists when negating', function() {
+        expect(evalLisp(['not', 0])).toEqual(false);
+        expect(evalLisp(['not', 42])).toEqual(false);
+        expect(evalLisp(['not', ['quote', []]])).toEqual(false);
+        expect(evalLisp(['not', ['quote', ['a']]])).toEqual(false);
+    });
+    it('test equality', function() {
+        expect(evalLisp(['eq?', 2, 2])).toEqual(true);
+        expect(evalLisp(['eq?', 2, 3])).toEqual(false);
+    });
+});
+
+describe('defined vars', function() {
+    it('can be accessed later on while in scope', function() {
+        expect(evalLisp(['begin', ['define', 'x', 1337],
+                                  'x'])).toEqual(1337);
+    });
+    it('can build on prior definitions', function() {
+        expect(evalLisp(['begin', ['define', 'a', 100],
+                                  ['define', 'b', ['+', 'a', 25]],
+                                  'b'])).toEqual(125);
+    });
+});
+
+describe('lambdas', function() {
+    it('can be run', function() {
+        expect(evalLisp([['lambda', [], 42]])).toEqual(42);
+    });
+    it('can manipulate an argument', function() {
+        expect(evalLisp([['lambda', ['x'], ['*', 'x', 'x']], 3]))
+            .toEqual(9);
+    });
+    it('can contain if statements', function() {
+        expect(evalLisp([['lambda', ['n'], ['if', ['eq?', 'n', 0], 1, 2]],
+                         0])).toEqual(1);
+        expect(evalLisp([['lambda', ['n'], ['if', ['eq?', 'n', 0], 1, 2]],
+                         35])).toEqual(2);
+    });
+});
+
+describe('the X combinator', function() {
+    it('is awesome', function() {
+        var parse = require('../parse').parseLisp;
+        var tokenize = require('../tokenize').tokenizeLisp;
+        var X_comb =
+            '(define X' +
+            '  (lambda (f)' +
+            '    ((lambda (x) (x x))' +
+            '      (lambda (x)' +
+            '        (f' +
+            '          (lambda (v) ((x x) v)))))))';
+        var fact =
+            '(define fact' +
+            '  (X (lambda (f) (lambda (n)' +
+            '    (if (eq? n 0)' +
+            '      1' +
+            '      (* n (f (- n 1))))))))';
+        // var expr = ['begin',
+        //             parse(tokenize(X_comb)),
+        //             parse(tokenize(fact)),
+        //             ['fact', 10]];
+        // expect(evalLisp(expr)).toEqual(3628800);
+        var bindings = {};
+        evalLisp(parse(tokenize(X_comb)), bindings);
+        expect(bindings.X).toBeDefined();
+        evalLisp(parse(tokenize(fact)), bindings);
+        expect(bindings.X).toBeDefined();
+        expect(bindings.fact).toBeDefined();
+        expect(evalLisp(['fact', 10], bindings)).toEqual(3628800);
+    });
+});
